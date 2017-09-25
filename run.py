c307eb22ce12c006a917eb526c85272acc00dfc6
guohailu <1948492265@qq.com>
2017-09-25 23:35:25 +0800

    no message

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from flask import Flask  
+from flask import render_template
 app=Flask(__name__)  
 @app.route('/')  
 def hello_world():  
-    return "Hello World!"  
+    return render_template('A16042forgiven.html') 
+@app.route('/details')  
+def details():
+	print 'hello hahahahah'
+	return render_template('index.html') 
 if __name__ == '__main__':  
-    app.run()  
+    app.run(debug=True)  
